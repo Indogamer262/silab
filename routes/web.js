ee37e7c826d7ac2d807f0c5dba01a69469b4fe34
2472008 Christian <88981963+Indogamer262@users.noreply.github.com>
2026-05-20 12:01:59 +0700

    update readme

--- routes/web.js
+++ routes/web.js
@@ -6,15 +6,15 @@ router.use(express.urlencoded({extended: false}))
 router.post('/receiver', (req, res) => {
     const name = req.body.name
     const email = req.body.email
-    res.status(200).render('received', {name, email})
+    //res.status(200).render('received', {name, email})
 })
 
 router.get('/form', (req, res) => {
-    res.status(200).render('forms')
+    //res.status(200).render('forms')
 })
 
 router.get('/', (req, res) => {
-    res.status(200).render('index')
+   // res.status(200).render('index')
 })
 
 
